818a37179dab315831bf85c06bf0a321317e728b
FlorianTechie <florencesylvanus@gmail.com>
2023-04-27 04:41:39 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_most_numbers - checks for checks for a digit (0 thrugh 9).
+* Return: Always return 0.
+*/
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+	}
+	_putchar('\n');
+}
